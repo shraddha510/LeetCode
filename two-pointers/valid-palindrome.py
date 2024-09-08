9867deaaa32c210fef52792e2298cb392f989014
Shraddha Patel <47129026+shraddha510@users.noreply.github.com>
2024-09-08 12:21:31 -0500

    LC-valid-palindrome [Runtime: 31 ms; Memory: 12.5 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = s.lower()
         l = 0
         r = len(s) - 1
 
@@ -13,7 +12,7 @@ class Solution(object):
                 l += 1
             elif not s[r].isalnum():
                 r -= 1
-            elif s[l] == s[r]:
+            elif s[l].lower() == s[r].lower():
                 l += 1
                 r -= 1
             else:
