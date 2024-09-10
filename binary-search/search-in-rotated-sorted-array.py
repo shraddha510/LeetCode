162c33d950e4386fcdaaf8ccc5dbfbaf6a4e59bc
Shraddha Patel <47129026+shraddha510@users.noreply.github.com>
2024-09-10 15:15:35 -0500

    LC-search-in-rotated-sorted-array [Runtime: 31 ms; Memory: 11.9 MB]

--- binary-search/search-in-rotated-sorted-array.py
+++ binary-search/search-in-rotated-sorted-array.py
@@ -10,18 +10,24 @@ class Solution(object):
 
         while l <= r:
             mid = l + (r - l) // 2
-            if target == nums[mid]:
+
+            # If we found the target
+            if nums[mid] == target:
                 return mid
 
-            if target < nums[mid]:
-                if nums[mid] < r:
+            # Check if the left half is sorted
+            if nums[l] <= nums[mid]:
+                # Target is in the left half
+                if nums[l] <= target < nums[mid]:
                     r = mid - 1
-                elif nums[mid] > r:
+                else:
                     l = mid + 1
-            elif target > nums[mid]:
-                if nums[mid] < r:
+            # Otherwise, the right half must be sorted
+            else:
+                # Target is in the right half
+                if nums[mid] < target <= nums[r]:
                     l = mid + 1
-                elif nums[mid] > r:
+                else:
                     r = mid - 1
-        return -1
-        
+
+        return -1  # Target not found
