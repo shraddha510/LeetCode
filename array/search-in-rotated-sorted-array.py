e17c7d7122e830d3d7eaec4cf09ea47f2267799a
Shraddha Patel <47129026+shraddha510@users.noreply.github.com>
2024-09-10 15:16:25 -0500

    LC-search-in-rotated-sorted-array [Runtime: 14 ms; Memory: 12 MB]

--- array/search-in-rotated-sorted-array.py
+++ array/search-in-rotated-sorted-array.py
@@ -5,29 +5,27 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        l = 0
-        r = len(nums) - 1
+        l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = l + (r - l) // 2
-
-            # If we found the target
-            if nums[mid] == target:
+            mid = (l + r) // 2
+            if target == nums[mid]:
                 return mid
 
-            # Check if the left half is sorted
+            # left sorted portion
             if nums[l] <= nums[mid]:
-                # Target is in the left half
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
-                else:
+                if target > nums[mid] or target < nums[l]:
                     l = mid + 1
-            # Otherwise, the right half must be sorted
+                else:
+                    r = mid - 1
+            # right sorted portion
             else:
-                # Target is in the right half
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
-                else:
+                if target < nums[mid] or target > nums[r]:
                     r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
 
-        return -1  # Target not found
+
+
+
