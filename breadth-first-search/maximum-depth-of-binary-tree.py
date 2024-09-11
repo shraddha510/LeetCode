068fb455a635a849a9e9b0b9c5223ad6de5b1c60
Shraddha Patel <47129026+shraddha510@users.noreply.github.com>
2024-09-11 13:02:54 -0500

    LC-maximum-depth-of-binary-tree [Runtime: 24 ms; Memory: 14.3 MB]

---
+++ breadth-first-search/maximum-depth-of-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        return res
+
+
+        
